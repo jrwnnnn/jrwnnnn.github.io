f7b22d49d1d45dc2e57f1602656541363f994754
Mark Jerwin <jrwn.xyz@gmail.com>
2024-05-02 11:14:35 +0800

    Refactored code for better debuggability.

---
+++ script/global.js
@@ -0,0 +1,37 @@
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-H16N9DYWLG');
+document.addEventListener("DOMContentLoaded", function() {
+    const license = document.getElementById("license_click_action");
+
+    license.addEventListener("click", function() {
+        window.location.href = "../pages/articles/license.html#navigation";
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const home = document.getElementById("home");
+    const stats = document.getElementById("stats");
+    const projects = document.getElementById("projects");
+    const blog = document.getElementById("blog");
+    const gallery = document.getElementById("gallery");
+    
+    home.addEventListener("click", function() {
+        window.location.href = "../pages/home.html";
+    });
+    stats.addEventListener("click", function() {
+        window.location.href = "../pages/404.html#navigation";
+    });
+    projects.addEventListener("click", function() {
+        window.location.href = "../pages/projects.html#navigation";
+    });
+    blog.addEventListener("click", function() {
+        window.location.href = "../pages/blog.html#navigation";
+    });
+    gallery.addEventListener("click", function() {
+        window.location.href = "../pages/gallery.html#navigation";
+    });
+
+});
