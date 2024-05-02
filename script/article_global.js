41acfceb054f6fe9b224c40cf3a2c1faf9284cbd
Mark Jerwin <jrwn.xyz@gmail.com>
2024-05-03 07:09:40 +0800

    Fixed bug causing broken #navigation and #license_click_action links.

---
+++ script/article_global.js
@@ -0,0 +1,38 @@
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-H16N9DYWLG');
+
+document.addEventListener("DOMContentLoaded", function() {
+    const license = document.getElementById("license_click_action");
+
+    license.addEventListener("click", function() {
+        window.location.href = "../articles/license.html#navigation";
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const home = document.getElementById("home");
+    const stats = document.getElementById("stats");
+    const projects = document.getElementById("projects");
+    const blog = document.getElementById("blog");
+    const gallery = document.getElementById("gallery");
+    
+    home.addEventListener("click", function() {
+        window.location.href = "../home.html";
+    });
+    stats.addEventListener("click", function() {
+        window.location.href = "../404.html#navigation";
+    });
+    projects.addEventListener("click", function() {
+        window.location.href = "../projects.html#navigation";
+    });
+    blog.addEventListener("click", function() {
+        window.location.href = "../blog.html#navigation";
+    });
+    gallery.addEventListener("click", function() {
+        window.location.href = "../gallery.html#navigation";
+    });
+
+});
