9f387827ecbf515398ddda4ae9e57442863cfbe0
Mark Jerwin <jrwn.xyz@gmail.com>
2024-05-03 19:16:05 +0800

    added script to gallery.html that dynamically adds images from a directory

---
+++ script/gallery.js
@@ -0,0 +1,16 @@
+fetch('../docs/assets/gallery/')
+            .then(response => response.text())
+            .then(text => {
+                const parser = new DOMParser();
+                const html = parser.parseFromString(text, 'text/html');
+                const images = Array.from(html.querySelectorAll('a[href$=".jpg"], a[href$=".jpeg"], a[href$=".png"], a[href$=".gif"]'));
+                const imagesContainer = document.getElementById('images_container');
+                images.forEach(image => {
+                    const url = image.getAttribute('href');
+                    const imgElement = document.createElement('img');
+                    imgElement.src = url;
+                    imgElement.alt = 'image_entry';
+                    imgElement.onclick = () => window.open(url, '_parent');
+                    imagesContainer.appendChild(imgElement);
+                });
+            });
