8fe560b0b5b9d42286fe00555b8aae028ef0683a
Mark Jerwin <jrwn.xyz@gmail.com>
2024-08-11 18:06:59 +0800

    Hotfix

--- script/projects.js
+++ script/projects.js
@@ -1,13 +1,10 @@
-function redirectToPage() {
-    const selectElement = document.getElementById("filterSelect");
-    const selectedOption = selectElement.value;
-  
-    const pageMap = {
-      "recent": "https://jrwnnnn.github.io/pages/projects.html#navigation",
-      "active": "https://jrwnnnn.github.io/pages/project_filters/active.html#navigation",
-      "finished": "https://jrwnnnn.github.io/pages/project_filters/finished.html#navigation",
-      "discontinued": "https://jrwnnnn.github.io/pages/project_filters/discontinued.html#navigation"
-    };
-  
-    window.location.href = pageMap[selectedOption];
-  }
+document.addEventListener('DOMContentLoaded', () => {
+  const selectElement = document.getElementById('filterSelect');
+
+  selectElement.addEventListener('change', function() {
+      const selectedValue = this.value;
+      if (selectedValue) {
+          window.location.href = selectedValue;
+      }
+  });
+});
