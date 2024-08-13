fdb867f02106a0621bdcbce3081b10c66ceb554c
Mark Jerwin <jrwn.xyz@gmail.com>
2024-08-13 10:42:51 +0800

    fix for the filter selection

--- script/projects.js
+++ script/projects.js
@@ -1,10 +1,13 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const selectElement = document.getElementById('filterSelect');
+function filter() {
+  var select = document.getElementById('filterSelect');
 
-  selectElement.addEventListener('change', function() {
-      const selectedValue = this.value;
-      if (selectedValue) {
-          window.location.href = selectedValue;
-      }
-  });
-});
+  if (select.value == "0" && window.location.href != "http://jrwnnnn.github.io/pages/projects.html#navigation") {
+    window.location.href = "http://jrwnnnn.github.io/pages/projects.html#navigation";
+  } else if (select.value == "1" && window.location.href != "http://jrwnnnn.github.io/pages/active.html#navigation") {
+    window.location.href = "http://jrwnnnn.github.io/pages/active.html#navigation";
+  } else if (select.value == "2" && window.location.href != "http://jrwnnnn.github.io/pages/finished.html#navigation") {
+    window.location.href = "http://jrwnnnn.github.io/pages/finished.html#navigation";
+  } else if (select.value == "3" && window.location.href != "http://jrwnnnn.github.io/pages/discontinued.html#navigation") {
+    window.location.href = "http://jrwnnnn.github.io/pages/discontinued.html#navigation";
+  }
+}
